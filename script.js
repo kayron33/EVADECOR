c231437205a15a21892557cd52c05ab3205f8cdc
kayron33 <129231712+kayron33@users.noreply.github.com>
2024-02-27 09:06:41 -0300

    telas adicionadas

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ thumbnails.forEach(thumbnail => {
         mainImg.src = thumbnail.src;
     });
 });
+
+
+var header = document.getElementById('header');
+var paginas = document.getElementById('paginas');
+var texto = document.getElementById('texto');
+var showSidebar = false;
+
+function toogleSidebar() {
+    showSidebar = !showSidebar;
+    if (showSidebar) {
+        paginas.style.marginLeft = '-10vw';
+        paginas.style.animationName = 'showSidebar';
+
+
+    }
+    else {
+        paginas.style.marginLeft = '-100vw';
+        paginas.style.animationName = '';
+    }
+}
