13c7f652f51dcfadcfde9c6832a2284b4102b24d
kayron33 <129231712+kayron33@users.noreply.github.com>
2024-04-01 19:49:47 -0300

    adicionado em todos os tintes

--- script.js
+++ script.js
@@ -26,3 +26,29 @@ function toogleSidebar() {
         paginas.style.animationName = '';
     }
 }
+
+//dialog aparecer a promoçaõ
+let modalSite=document.querySelector('dialog')
+let btnFechar=document.querySelectorAll('dialog button')
+
+let cont=0
+
+
+setTimeout(()=>{
+modalSite.showModal()
+
+}, 3000);
+//document.addEventListener('mouseleave',()=>{
+    //if(cont==0)
+
+
+   /// modalSite.showModal()
+//})
+
+btnFechar.forEach(button => {
+    button.addEventListener('click',()=>{
+        modalSite.close()
+        cont=1;
+    })
+})
+
